d263325eb3cb1ff56de88ecc93ac5d88ae12f797
rmsmrathnayake <rmsmrathnayake@students.nsbm.lk>
2020-08-13 11:12:00 -0300

    about page merged

---
+++ about.php
@@ -0,0 +1,221 @@
+
+<?php
+if(!isset($_SESSION)) 
+{ 
+	session_start(); 
+} 
+?>
+
+<?php include('server.php') ?>
+
+
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+  <link rel="icon" href="favicon.png" type="image/png">
+  <meta name="description" content="Sea side south part. The best hotel network.">
+<meta name="keywords" content="Sea side south park, Sea side hotel, Sea side, best hotels in sri lanka, hotels to stay">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+
+	<link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+	<link rel="stylesheet" href="css/animate.css">
+
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<link rel="stylesheet" href="css/aos.css">
+
+	<link rel="stylesheet" href="css/ionicons.min.css">
+
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<link rel="stylesheet" href="css/jquery.timepicker.css">
+
+
+	<link rel="stylesheet" href="css/flaticon.css">
+	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="css/style.css">
+
+	<link rel="stylesheet" type="text/css" href="css/login-style.css">
+	<link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="/about/style.css">
+
+    <title>About Us</title>
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar" style="background:#33313b !important;">
+		<div class="container">
+			<a class="navbar-brand" href="index.php">Sea Side South Park<span>Your best choice!</span></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+				aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="oi oi-menu"></span> Menu
+			</button>
+
+			<div class="collapse navbar-collapse" id="ftco-nav">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item"><a href="index.php" class="nav-link">HOME</a></li>
+					<li class="nav-item active"><a href="about.php" class="nav-link">ABOUT</a></li>
+					<li class="nav-item"><a href="contact.php" class="nav-link">CONTACT US</a></li>
+					<li class="nav-item"><a href="reserve.php" class="nav-link">RESERVE</a></li>
+					<li class="nav-item"><a href="login.php" class="nav-link">LOGIN</a></li>
+
+				</ul>
+			</div>
+		</div>
+    </nav>
+    
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            
+                <h1>ABOUT US</h1>
+
+            <hr class= "featurette-divider">
+
+            <P style="color:white !important;">It was another day on the Weligama beach in the early 2020s; they were<br>
+                turinators a visionary team of young lads with expotential desire to cater the<br>
+                supreme hospitality experience in Sri Lanka. The homely resort they built and<br>
+                named Sea Side South Park of just six rooms may have been small, but the<br>
+                heartfelt warmth it offered was immense. It all began with a humble desire to<br>
+                share the experiece of true hospitality of a Sri Lankan family with others, infusing<br>
+                the touch of sounsouth coastal wind. A simple dream which grew to an ever<br>
+                furthering vision that made us what we are today - on of the mostprestigious.
+            </P>
+            
+            </div>
+        </div>
+
+        <hr class= "featurette-divider">
+
+        <div class="row">
+            
+            <div class= "col" class="img-hover-zoom">
+                <img class="rounded-circle" src="/about/103641165_145975440347155_409190487583317192_o.jpg" alt="" width="140" height="140">
+                <h5>Rashan Hasaranga</h5>
+                <p></p>
+                <p>
+                    <a class= "btn btn-secondary" href="https://github.com/RashanH" role="button"> View detail</a>
+                </p>
+            </div>
+
+            <div class= "col">
+                <img class="rounded-circle" src="/about/110278074_1661474237360551_2573287637270735852_n.jpg" alt="" width="140" height="140">
+                <h5>Shashika Mihiran</h5>
+                <p></p>
+                <p>
+                    <a class= "btn btn-secondary" href="https://github.com/rmsmrathnayake" role="button"> View detail</a>
+                </p>
+            </div>
+
+            <div class= "col">
+                <img class="rounded-circle" src="/about/109967668_3132988263451821_1149281672305369712_o.jpg" alt="" width="140" height="140">
+                <h5>Shavindu Avishka</h5>
+                <p></p>
+                <p>
+                    <a class= "btn btn-secondary" href="https://github.com/ShavinduAvishka" role="button"> View detail</a>
+                </p>
+            </div>
+
+            <div class= "col">
+                <img class="rounded-circle" src="/about/79677653_2488146931441717_2593914072143495168_o.jpg" alt="" width="140" height="140">
+                <h5>Tharuka Fernando</h5>
+                <p></p>
+                <p>
+                    <a class= "btn btn-secondary" href="https://github.com/RukmalFernando" role="button"> View detail</a>
+                </p>
+            </div>
+            
+            <div class= "col">
+                <img class="rounded-circle" src="/about/55674715_2048174761967140_1094819101127213056_n.jpg" alt="" width="140" height="140">
+                <h5>Kavinda Dilshan</h5>
+                <p></p>
+                <p>
+                    <a class= "btn btn-secondary" href="https://github.com/KDTechniques" role="button"> View detail</a>
+                </p>
+            </div>
+
+
+        </div>
+    </div>
+
+    <footer class="ftco-footer bg-bottom" style="background-image: url(images/footer-bg.jpg);">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4">
+                        <h2 class="ftco-heading-2">Sea Side South Park</h2>
+                        <p style="color: #a2a2a2 !important; text-align: left;">The benchmark for supreme, seafront
+                            experience in Sri Lanka.</p>
+                        <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                            <li class="ftco-animate"><a target="_blank"
+                                    href="https://twitter.com/seasidesouthparknsbm"><span
+                                        class="icon-twitter"></span></a></li>
+                            <li class="ftco-animate"><a target="_blank"
+                                    href="https://facebook.com/seasidesouthparknsbm"><span
+                                        class="icon-facebook"></span></a></li>
+                            <li class="ftco-animate"><a target="_blank"
+                                    href="https://instagram.com/seasidesouthparknsbm"><span
+                                        class="icon-instagram"></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4 ml-md-5">
+                        <h2 class="ftco-heading-2">Links</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="index.php" class="py-2 d-block">Homepage</a></li>
+                            <li><a href="about.php" class="py-2 d-block">About</a></li>
+                            <li><a href="contact.php" class="py-2 d-block">Contact</a></li>
+                            <li><a href="network.php" class="py-2 d-block">Our network</a></li>
+                            <li><a href="reserve.php" class="py-2 d-block">Reserve</a></li>
+                            <li><a href="login.php" class="py-2 d-block">Account</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4">
+                        <h2 class="ftco-heading-2">Have a Questions?</h2>
+                        <div class="block-23 mb-3">
+                            <ul>
+                                <li><span class="icon icon-map-marker"></span><span class="text">38/02, Temple Road,
+                                        Weligama, Sri Lanka.</span></li>
+                                <li><a><span class="icon icon-phone"></span><span class="text">+94 11
+                                            7745730</span></a></li>
+                                <li><a><span class="icon icon-envelope"></span><span
+                                            class="text">reservations@nsbm.cloud</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+
+                    <p style="color: #a2a2a2 !important; text-align: center;">
+                        Copyright 2020 - Sea Side South Park
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
